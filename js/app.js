48d945fe06a599d16f9997393c131b7d880d8f44
Davi Marques <dmarques60@gmail.com>
2025-05-07 23:12:04 -0300

    Primeiro commit.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const task_input = document.getElementById("container_input");
+const addtask_button = document.getElementById("container_button");
+const task_list = document.getElementById("container_task-list");
+
+addtask_button.addEventListener('click', function() {
+    const task_text = task_input.value.trim();
+    
+    
+    
+});
+
